33b3036d962433a986b5fb0a025a1bf02aa9a47e
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-06-20 14:59:18 -0500

    chore: add more api constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,13 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.uniride.com';
-  static const String googleMapApiKey = 'ghghgh';
+  static const String googleMapApiKey = '';
+  static const String googleMapSearchLocation = 'https://maps.googleapis.com/maps/api/place/autocomplete';
+
+
+
+  //ApiGeolocation
+  static const String apiGeolocation = 'https://geocode.googleapis.com/v4beta/geocode/address';
 }
+
+
+//https://maps.googleapis.com/maps/api/place/autocomplete/json?input=upc&key=AIzaSyAmc--LhYp4c9_v4HrU4bITMBx6Dx1tVO0&language=es&components=country:pe
