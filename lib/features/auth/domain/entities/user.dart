33f08dc14c532d668abd27c68bc66d91d36e7d44
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-04 11:33:00 -0500

    refactor:  enhance user entity and role

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,10 +1,20 @@
-import 'package:uniride_driver/features/auth/domain/entities/user_role.dart';
+import 'package:equatable/equatable.dart';
+import 'package:uniride_driver/features/auth/domain/entities/role.dart';
+import 'package:uniride_driver/features/auth/domain/entities/user_status.dart';
 
-class User {
+class User extends Equatable {
   final String id;
-  final String name;
   final String email;
-  final UserRole role;
+  final UserStatus status;
+  final List<Role> roles;
 
-  User({required this.id, required this.name, required this.email, required this.role});
+  const User({
+    this.id = '1',
+    this.email = '',
+    this.status = UserStatus.notVerified,
+    this.roles = const [Role.driver],
+  });
+
+  @override
+  List<Object> get props => [id, email, status, roles];
 }
