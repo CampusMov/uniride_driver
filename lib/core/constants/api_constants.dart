086ed333f54b1a281da5bdf2e5151f76d7f1e854
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-04 11:29:48 -0500

    chore: update base url and add more links for connections

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,3 +1,8 @@
 class ApiConstants {
-  static const String baseUrl = 'https://api.uniride.com';
+  static const String baseUrl = 'http://10.0.2.2:8765/';
+  static const String webSocketUrl = 'ws://10.0.2.2:8765/';
+  static const String authServiceName = 'iam-service';
+  static const String profileServiceName = 'profile-service';
+  static const String routingMatchingServiceName = 'matching-routing-service';
+  static const String inTripCommunicationServiceName = 'in-trip-communication-service';
 }
