05f95bc193c80892c237e480bea9a61e019acfd8
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-04 13:12:45 -0500

    feat: add screens routes

---
+++ lib/core/navigation/screens_routes.dart
@@ -0,0 +1,7 @@
+class ScreensRoutes {
+  static const String home = '/';
+  static const String welcome = '/welcome';
+  static const String enterInstitutionalEmail = '/enter-institutional-email';
+  static const String enterVerificationCode = '/enter-verification-code';
+  static const String registerProfile = '/register-profile';
+}
