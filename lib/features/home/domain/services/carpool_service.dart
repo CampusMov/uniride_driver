906b8d5e5c31c70481c77cf4144f91325d93fa91
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-06 15:42:55 -0500

    feat: add carpool service

---
+++ lib/features/home/domain/services/carpool_service.dart
@@ -0,0 +1,6 @@
+import 'package:uniride_driver/features/home/data/model/carpool_request_model.dart';
+import 'package:uniride_driver/features/home/data/model/carpool_response_model.dart';
+
+abstract class CarpoolService {
+  Future<CarpoolResponseModel> createCarpool(CarpoolRequestModel request);
+}
