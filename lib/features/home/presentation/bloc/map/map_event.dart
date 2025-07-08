3031c24a573b17f57e231096be7ffeaa5809c3c6
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-08 01:19:02 -0500

    feat: update map bloc, state and event

--- lib/features/home/presentation/bloc/map/map_event.dart
+++ lib/features/home/presentation/bloc/map/map_event.dart
@@ -1,38 +1,164 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:uniride_driver/features/home/data/model/route_request_model.dart';
 
-abstract class MapEvent {
+import '../../../domain/entities/map/marker_data.dart';
+
+abstract class MapEvent extends Equatable {
   const MapEvent();
+
+  @override
+  List<Object?> get props => [];
 }
 
-class InitialMap extends MapEvent{
+// Init event for the map
+class MapInitialized extends MapEvent {
+  final GoogleMapController controller;
 
-  final LatLng destinationPosition;
-  final LatLng initialPosition;
+  const MapInitialized(this.controller);
 
-  InitialMap({
-    required this.destinationPosition,
-    required this.initialPosition,
-  });
+  @override
+  List<Object?> get props => [controller];
 }
 
-//https://maps.googleapis.com/maps/api/directions/json?origin=-12.117642,-77.000320&destination=-12.111348,-77.000920&key=AIzaSyAmc--LhYp4c9_v4HrU4bITMBx6Dx1tVO0
+// Get user location
+class GetUserLocation extends MapEvent {}
 
+// Center the map to a specific location
+class CenterMap extends MapEvent {
+  final LatLng location;
+  final double zoom;
 
+  const CenterMap(this.location, {this.zoom = 15.0});
 
-class AddMarker extends MapEvent{
+  @override
+  List<Object?> get props => [location, zoom];
+}
+
+// Add a single marker
+class AddMarker extends MapEvent {
+  final String markerId;
   final LatLng position;
-  const AddMarker(this.position);
-}
+  final String title;
+  final String snippet;
+  final BitmapDescriptor? icon;
+  final VoidCallback? onTap;
 
-class CenterOnUserLocation extends MapEvent{
-  const CenterOnUserLocation();
-}
-
-class GetRoute extends MapEvent {
-  final RouteRequestModel routeRequestModel;
-
-  const GetRoute({
-    required this.routeRequestModel,
+  const AddMarker({
+    required this.markerId,
+    required this.position,
+    required this.title,
+    this.snippet = '',
+    this.icon,
+    this.onTap,
   });
+
+  @override
+  List<Object?> get props => [markerId, position, title, snippet, icon];
+}
+
+// Add multiple markers
+class AddMarkers extends MapEvent {
+  final List<MarkerData> markers;
+
+  const AddMarkers(this.markers);
+
+  @override
+  List<Object?> get props => [markers];
+}
+
+// Remove a single marker
+class RemoveMarker extends MapEvent {
+  final String markerId;
+
+  const RemoveMarker(this.markerId);
+
+  @override
+  List<Object?> get props => [markerId];
+}
+
+// Remove multiple markers
+class ClearMarkers extends MapEvent {}
+
+// Create a polyline with given points
+class CreatePolyline extends MapEvent {
+  final String polylineId;
+  final List<LatLng> points;
+  final Color color;
+  final double width;
+  final List<PatternItem>? patterns;
+
+  const CreatePolyline({
+    required this.polylineId,
+    required this.points,
+    this.color = const Color(0xFF2196F3),
+    this.width = 5.0,
+    this.patterns,
+  });
+
+  @override
+  List<Object?> get props => [polylineId, points, color, width, patterns];
+}
+
+// Remove a polyline by its ID
+class RemovePolyline extends MapEvent {
+  final String polylineId;
+
+  const RemovePolyline(this.polylineId);
+
+  @override
+  List<Object?> get props => [polylineId];
+}
+
+// Clear all polylines from the map
+class ClearPolylines extends MapEvent {}
+
+// Fit all markers to the map view with optional padding
+class FitMarkersToMap extends MapEvent {
+  final EdgeInsets padding;
+
+  const FitMarkersToMap({this.padding = const EdgeInsets.all(100.0)});
+
+  @override
+  List<Object?> get props => [padding];
+}
+
+// Start navigation from origin to destination with optional waypoints
+class StartNavigation extends MapEvent {
+  final LatLng origin;
+  final LatLng destination;
+  final List<LatLng>? waypoints;
+
+  const StartNavigation({
+    required this.origin,
+    required this.destination,
+    this.waypoints,
+  });
+
+  @override
+  List<Object?> get props => [origin, destination, waypoints];
+}
+
+// Stop ongoing navigation
+class StopNavigation extends MapEvent {}
+
+// Update the user's current location on the map
+class UpdateUserLocation extends MapEvent {
+  final LatLng location;
+  final double heading;
+
+  const UpdateUserLocation(this.location, {this.heading = 0.0});
+
+  @override
+  List<Object?> get props => [location, heading];
+}
+
+// Change the map type (normal, satellite, terrain, hybrid)
+class ChangeMapType extends MapEvent {
+  final MapType mapType;
+
+  const ChangeMapType(this.mapType);
+
+  @override
+  List<Object?> get props => [mapType];
 }
