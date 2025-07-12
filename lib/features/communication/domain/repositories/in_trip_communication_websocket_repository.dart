3182c039fd9d81aff889afee0af35ffdc7f5be5a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-12 03:52:56 -0500

    chore: remove no used files

--- lib/features/communication/domain/repositories/in_trip_communication_websocket_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/message.dart';
-
-abstract class InTripCommunicationWebSocketRepository {
-  Future<void> connectSession();
-  Stream<Message> subscribeToChat(String chatId);
-  Future<void> sendMessage(String chatId, String senderId, String content);
-  Future<void> disconnectSession();
-  Future<void> awaitConnectionReady();
-}
